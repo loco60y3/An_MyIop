3b1dec872c74f8c60d8de36ecfd13de6798bb230
我也想吃汤圆 <3038598754@qq.com>
2024-05-29 20:07:19 +0800

    删除评论

--- js/index.js
+++ js/index.js
@@ -8,20 +8,29 @@ AV.init({
 const textTEST = document.getElementById("text");
 const commentList = document.getElementById("comment");
 
-let testDomeQuery = new AV.Query('TestDome'); // 修正变量名为testDomeQuery
+let testDomeQuery = new AV.Query('TestDome'); 
 
 // 查询操作
 testDomeQuery.find().then((rows) => {
+	console.log(rows)
     for (var i = 0; i < rows.length; i++) {
         let { attributes } = rows[i];
         let li = document.createElement('li');
-        li.textContent = attributes.Text;
+		li.classList.add("commlist")
+        // li.textContent = attributes.Text;
+        // console.log(li);
+        li.innerHTML += `${attributes.Text} 	---   <a class="del" id="${rows[i].id}">删除</a>`
+
         if (commentList.firstChild) {
             commentList.insertBefore(li, commentList.firstChild);
         } else {
             commentList.appendChild(li);
         }
     }
+	
+	//添加删除操作
+	delrow()
+	
 }).catch(error => {
     console.error('查询失败：', error);
 });
@@ -33,15 +42,36 @@ function setHigh() {
         newComms.set("Text", textTEST.value);
         newComms.save().then((comm) => {
             console.log("保存成功", comm);
-            textTEST.value = ``;
+            // textTEST.value = ``;
             let li = document.createElement('li');
-            li.textContent = textTEST.value;
+            // li.textContent = textTEST.value;
+			li.innerHTML += `${textTEST.value} 	---   <a class="del" id="${comm.id}">删除</a>`
+			
+            // li.innerHtml =`${textTEST.value}`
             commentList.insertBefore(li, commentList.firstChild);
+			delrow()
         }, (error) => {
             console.log(error);
         });
     }
 }
+
+function delrow(){
+	let dels = document.querySelectorAll(".del")
+	dels.forEach((i)=>{
+		i.addEventListener("click",function(){
+			console.log("删除id为",i.id)
+			const TestDome = AV.Object.createWithoutData("TestDome", i.id);
+			
+			TestDome.destroy().then((i)=>{
+				console.log("删除成功",i)
+			// 	console.log(this.parenNode)
+			this.parentNode.remove();
+			});
+		})
+	})
+}
+
 function refto(){
     location.reload()
 }
